cf19fba97b1e3babff5315efa1acacb6cc3cc3a2
Code-WithSujith <sujith.innovatestech@gmail.com>
2025-08-12 23:34:35 +0530

    initial commit for tamil book review

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Header } from './component';
+import AllRoutes from './routes/AllRoutes';
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <AllRoutes/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+
+export function Header() {
+  return (
+    <div className="container-fluid bg-dark text-white p-2">
+        <div className="row">
+            <div className="col-2">New News</div>
+            <div className="col-5">15/08/2025 -- Indian Independance Day - 2025</div>
+            <div className="col-5">
+                <div className="container p-2">soem contents will be come here</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
